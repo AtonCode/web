e25c4b900d8ff52845163e06e1869276eee60b62
AtonCode <49330231+AtonCode@users.noreply.github.com>
2022-09-08 18:41:34 -0500

    Eureka

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MicroServicioPanteraService } from './micro-servicio-pantera.service';
 
@@ -9,9 +9,11 @@ import { MicroServicioPanteraService } from './micro-servicio-pantera.service';
 export class AuthGuard implements CanActivate {
 
   microServicioPanteraService: MicroServicioPanteraService | undefined;
+  router: Router | undefined;
 
-  constructor(_microServicioPanteraService: MicroServicioPanteraService) {
+  constructor(_microServicioPanteraService: MicroServicioPanteraService, router: Router,) {
     this.microServicioPanteraService = _microServicioPanteraService;
+    this.router = router;
   }
 
   canActivate(
@@ -19,10 +21,18 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     
     if (this.microServicioPanteraService?.validarToken() == true) {
+      
       return true;
+    }else{
+      //alert('You need to login first');
+     this.router?.navigate(['/login']);     
     }
 
     return false;
   }
   
 }
+function toast(arg0: string) {
+  throw new Error('Function not implemented.');
+}
+
